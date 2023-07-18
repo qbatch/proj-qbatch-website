8fad2b2a8b88d9ff257c018b8592ef437d6ecb18
ali-haider-qbatch <ali@qbatch.com>
2023-07-18 17:16:19 +0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `qbatch-official-site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
