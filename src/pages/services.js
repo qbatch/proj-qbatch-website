c3c9f6ee66cc2d069d291432aa9958b0e2e57be8
Ahmad Siddiqui <ahmad.siddiqui@qbatch.com>
2023-08-22 13:53:09 +0500

    added demo services section on section page

--- src/pages/services.js
+++ src/pages/services.js
@@ -6,11 +6,13 @@ import Divider from "../components/UiComponent/Divider";
 import ServicesBanner from "../components/PagesComponent/ServicesBanner";
 import Collaboration from "../components/PagesComponent/Collaboration";
 import StartProject from "../components/PagesComponent/StartProject";
+import DemoServices from "../components/PagesComponent/DemoServices"
 
 const Services = () => {
   return (
     <Layout>
        <ServicesBanner /> 
+       <DemoServices/>
        <Collaboration />
        <Divider margin="0px" />
        <StartProject />
