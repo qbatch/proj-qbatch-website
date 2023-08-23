f8260f53167662b74dff1199bbf2ec887dd71afd
ali-haider-qbatch <ali@qbatch.com>
2023-08-23 17:26:08 +0500

    reset expand view

--- src/components/PagesComponent/DemoServices/index.js
+++ src/components/PagesComponent/DemoServices/index.js
@@ -1,59 +1,87 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DemoServiceWrapper from "./style";
 import ExpandableView from "./style";
 import PrimaryButton from "../../UiComponent/PrimaryButton";
 
 import Container from "../../UiComponent/Container";
+  const stories = [
+    {
+      logo: "/ecom-circle.svg",
+      title: "Ecom Circles",
+      description:
+        "We created an all-in-one E-commerce Management Software trusted by 3000+ Amazon and Walmart sellers to organize and streamline their drop-shipping business.",
+      badge: ["Automation", "Saas", "Warehousing"],
+      link: "/",
+      primary: false,
+    },
+    {
+      logo: "/medical.svg",
+      title: "My IMC",
+      description:
+        "A convenient clinical app with 50k+ downloads for patients to book appointments withdoctors and manage medical history from anywhere, anytime.",
+      badge: ["Mobile App", "HealthTech", "Data Management"],
+      link: "/",
+      primary: false,
+    },
+    {
+      logo: "/mediacl-primary.svg",
+      title: "IMC Women’s Health",
+      description:
+        "A personalized clinical app for women housed by board-certified gynaecologists for both routine checkups and specialized care.",
+      badge: ["Mobile App", "HealthTech", "Data Management"],
+      link: "/",
+      primary: true,
+    },
+    {
+      logo: "/sales-support.svg",
+      title: "Sales Support",
+      description:
+        "We’ve created an immaculate Amazon Wholesale Kickstarter that helped launch 1000+ businesses on Amazon. The platform features a Repricer for automated pricing and a catalog analyzer for better forecasting and profit/loss analysis. While the reimbursements removal and FBA forecasting elevated their business off the ground.",
+      badge: ["Automation", "Saas", "Warehousing"],
+      link: "/",
+      primary: true,
+    },
+    {
+      logo: "/one-apply.svg",
+      title: "One App",
+      description:
+        "We developed a powerful E-commerce marketplace that manages the daily inventory and stock data of above 50 different stores. The automated & algorithmic Repricer allows optimal order management, total control over centralized accounts, better profit/loss calculation, & reliable FBA forecasting.",
+      badge: ["E-commerce", "Automation", "SaaS"],
+      link: "/",
+      primary: false,
+    },
+    {
+      logo: "/tripexpert.svg",
+      title: "Tripexpert",
+      description:
+        "We offered maintenance and support to a hefty travel experience expert for a faster-loading website amid massive data operations. We introduced booking.com, Agoda, and ElasticSearc as reliable integrations to enable easy search. While data scraping improved overall platform performance. ",
+      badge: ["Maintenance & Support", "Integrations", "Data Scraping", "Travel"],
+      link: "/",
+      primary: false,
+    },
+  ];
 
 
 const Index = () => {
-    const stories = [
-      {
-        logo: "/ecom-circle.svg",
-        title: "Ecom Circles",
-        description:
-          "We created an all-in-one E-commerce Management Software trusted by 3000+ Amazon and Walmart sellers to organize and streamline their drop-shipping business.",
-        badge: ["Automation", "Saas", "Warehousing"],
-        link: "/",
-        primary: false,
-      },
-      {
-        logo: "/medical.svg",
-        title: "My IMC",
-        description:
-          "A convenient clinical app with 50k+ downloads for patients to book appointments withdoctors and manage medical history from anywhere, anytime.",
-        badge: ["Mobile App", "HealthTech", "Data Management"],
-        link: "/",
-        primary: false,
-      },
-      {
-        logo: "/mediacl-primary.svg",
-        title: "IMC Women’s Health",
-        description:
-          "A personalized clinical app for women housed by board-certified gynaecologists for both routine checkups and specialized care.",
-        badge: ["Mobile App", "HealthTech", "Data Management"],
-        link: "/",
-        primary: true,
-      },
-      {
-        logo: "/sales-support.svg",
-        title: "Sales Support",
-        description:
-          "We’ve created an immaculate Amazon Wholesale Kickstarter that helped launch 1000+ businesses on Amazon. The platform features a Repricer for automated pricing and a catalog analyzer for better forecasting and profit/loss analysis. While the reimbursements removal and FBA forecasting elevated their business off the ground.",
-        badge: ["Automation", "Saas", "Warehousing"],
-        link: "/",
-        primary: true,
-      },
-      {
-        logo: "/ecom-circle.svg",
-        title: "IMC Women’s Health",
-        description:
-          "A personalized clinical app for women housed by board-certified gynaecologists for both routine checkups and specialized care.",
-        badge: ["Amazon", "Saas", "Platform Management", "Web App"],
-        link: "/",
-        primary: false,
-      },
-    ];
+    const [width, setWidth] = useState("");
+    const [columnSlice, setColumnSlice] = useState(3);
+
+    useEffect(() => {
+      if (typeof window !== "undefined") {
+        setWidth(window.innerWidth);
+
+        const handleResize = () => {
+          setWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }
+    }, []);
+
   useEffect(() => {
     const wrappers = document.querySelectorAll(".wrapper");
 
@@ -61,13 +89,12 @@ const Index = () => {
       const columns = wrapper.querySelectorAll(".column");
       if (columns.length === 3) {
         const lastColumn = wrapper.lastElementChild;
-
         const handleMouseOver = () => {
-          wrapper.style.transform = "translateX(-33.33%)";
+          wrapper.style.transform = "translateX(-32%)";
         };
 
         const handleMouseOut = () => {
-          wrapper.style.transform = "none";
+           wrapper.style.transform = "translateX(0)";
         };
 
         lastColumn.addEventListener("mouseover", handleMouseOver);
@@ -79,20 +106,22 @@ const Index = () => {
         };
       }
     });
-  }, []);
+  }, [columnSlice]);
+
+  useEffect(() => {
+    if (width) {
+      const slice = width > 1400 ? 3 : 2;
+      setColumnSlice(slice);
+    }
+  }, [columnSlice, width]);
 
   const renderColumns = () => {
     const columns = [];
-
-    for (let i = 0; i < stories.length; i += 3) {
-      const columnSet = stories.slice(i, i + 3);
-
+    for (let i = 0; i < stories.length; i += columnSlice ) {
+      const columnSet = stories.slice(i, i + columnSlice)
       const columnElements = columnSet.map((item, index) => (
-        <div className={`column ${item.primary && "primary" }`} key={index}>
+        <div className={`column ${item.primary ? "primary" : ""} ${columnSlice === 2 ? "w-50" : ""}`} key={index}>
           <div className="column-inner">
-            <div className="column-inner-left">
-              <img src="/ecom-circle.png" className="image-logo" alt="no-image" />
-            </div>
             <div className="column-inner-right">
               <img className="logo" src={item.logo} />
               <h3>{item.title}</h3>
@@ -104,10 +133,12 @@ const Index = () => {
               </div>
               <PrimaryButton text="View Details" color={i == 2 ? "#fff" : ""} />
             </div>
+            <div className="column-inner-left">
+              <img src="/ecom-circle.png" className="image-logo" alt="no-image" />
+            </div>
           </div>
         </div>
       ));
-
       const wrapperElement = (
         <div className="wrapper" key={i}>
           {columnElements}
--- src/components/PagesComponent/DemoServices/style.js
+++ src/components/PagesComponent/DemoServices/style.js
@@ -2,17 +2,68 @@ import styled from "styled-components";
 import { media } from "../../../theme/media-mixins";
 
 const DemoServiceWrapper = styled.div`
-overflow: hidden;
+  overflow: hidden;
   .wrapper {
     display: flex;
     transition: transform 0.6s;
+    gap: 14px;
     .column {
-      min-width: 33.33%;
-      padding: 10px;
+      min-width: 33%;
+      margin-bottom: 16px;
       position: relative;
       transition: min-width 0.6s;
       z-index: 1;
       cursor: pointer;
+      .column-inner {
+        display: flex;
+        align-items: center;
+        gap: 16px;
+        border: 1px solid ${({ theme }) => theme.colors.dividerColor};
+        background-color: ${({ theme }) => theme.colors.whiteColor};
+        border-top-left-radius: 24px;
+        border-top-right-radius: 24px;
+        border-bottom-left-radius: 24px;
+        padding: 47px;
+        height: 524px;
+        .column-inner-left {
+          img {
+            visibility: hidden;
+            transition: visibility 0.6s;
+            width: 0;
+          }
+        }
+        .column-inner-right {
+          max-width:350px;
+          min-width:350px;
+          .logo {
+            margin-bottom: 26px;
+          }
+          h3 {
+            margin-bottom: 18px;
+          }
+          .paragraph {
+            color: ${({ theme }) => theme.colors.textMedium};
+            font-size: ${({ theme }) => theme.fonts.baseFontSize};
+            max-width: 356px;
+            line-height: 24px;
+            margin-bottom: 29px;
+          }
+          .badge {
+            display: flex;
+            gap: 8px;
+            padding: 0;
+            flex-wrap: wrap;
+            margin-bottom: 16px;
+            span {
+              color: #7f8f97;
+              font-weight: 400;
+              border: 1px solid #7f8f97;
+              padding: 4px 7px;
+              border-radius: 2px;
+            }
+          }
+        }
+      }
       &.primary {
         .column-inner {
           background-color: ${({ theme }) => theme.colors.secondaryColor};
@@ -32,64 +83,15 @@ overflow: hidden;
           }
         }
       }
-      .column-inner {
-        display: flex;
-        align-items: center;
-        gap: 16px;
-        border: 1px solid ${({ theme }) => theme.colors.dividerColor};
-        background-color: ${({ theme }) => theme.colors.whiteColor};
-        border-top-left-radius: 24px;
-        border-top-right-radius: 24px;
-        border-bottom-left-radius: 24px;
-        padding: 47px;
-        height: 524px;
-        .column-inner-left {
-          img {
-            opacity: 0;
-            transition: opacity 0.6s;
-            width: 0;
-          }
-        }
-        .logo {
-          margin-bottom: 26px;
-        }
-        h3 {
-          margin-bottom: 18px;
-        }
-        .paragraph {
-          color: ${({ theme }) => theme.colors.textMedium};
-          font-size: ${({ theme }) => theme.fonts.baseFontSize};
-          max-width: 356px;
-          line-height: 24px;
-          margin-bottom: 29px;
-        }
-        .badge {
-          display: flex;
-          gap: 8px;
-          padding: 0;
-
-          span {
-            color: #7f8f97;
-            font-weight: 400;
-            border: 1px solid #7f8f97;
-            padding: 4px 7px;
-            border-radius: 2px;
-          }
-        }
-        button {
-          position: absolute;
-          bottom: 46px;
-        }
-      }
       &:hover {
-        min-width: 66.33%;
-        .column-inner {
-          .column-inner-left {
-            img {
-              opacity: 1;
-              height: 400px;
-              width: 400px;
-            }
+        min-width: 66%;
+        &.w-50 {
+          min-width: 100%;
+        }
+        .column-inner-left{
+          img{
+            visibility:visible ;
+            width:300px;
           }
         }
       }
