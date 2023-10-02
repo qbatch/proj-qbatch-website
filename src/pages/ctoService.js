6d7d2a093c236f34487b02f83f80dadd48adee7d
ali-haider-qbatch <ali@qbatch.com>
2023-10-02 14:10:18 +0500

    hire-a-cto-complete

--- src/pages/ctoService.js
+++ src/pages/ctoService.js
@@ -4,11 +4,13 @@ import SEO from '../components/Seo';
 import Layout from '../components/Layout/layout';
 import StartProject from '../components/PagesComponent/StartProject';
 import CtoBanner from '../components/PagesComponent/CtoBanner';
+import HireCto from '../components/PagesComponent/HireCto/index'
 
 const CtoService = () => {
   return (
     <Layout>
       <CtoBanner />
+      <HireCto />
       <StartProject />
     </Layout>
   )
