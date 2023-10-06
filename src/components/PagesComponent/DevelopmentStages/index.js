77a685fedcc69d4fdf70ea945689ae28e524e8bd
imran-ali-qbatch <imran.ali@qbatch.com>
2023-10-06 17:59:35 +0500

    scroll issue resolved

--- src/components/PagesComponent/DevelopmentStages/index.js
+++ src/components/PagesComponent/DevelopmentStages/index.js
@@ -10,7 +10,17 @@ import { sliderItems } from "../../../constants";
 const DevelopmentStages = () => {
   const sliderRef = useRef(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const scrollRef = useRef(null);
 
+  const scrollToSectionStart = () => {
+    if (scrollRef.current) {
+      const sectionTop = scrollRef.current.offsetTop;
+      window.scrollTo({
+        top: sectionTop,
+        behavior: "smooth",
+      });
+    }
+  };
   const sliderSettings = {
     dots: false,
     infinite: false,
@@ -49,6 +59,7 @@ const DevelopmentStages = () => {
         if (!isAtLastSlide) {
           e.preventDefault();
           sliderRef.current.slickNext();
+          scrollToSectionStart();
         } else {
           // Enable window scroll when at the last slide
           document.body.style.overflow = "auto";
@@ -61,6 +72,7 @@ const DevelopmentStages = () => {
         } else {
           e.preventDefault();
           sliderRef.current.slickPrev();
+          scrollToSectionStart();
         }
       }
     };
@@ -70,81 +82,87 @@ const DevelopmentStages = () => {
     sliderElement.addEventListener("wheel", handleMouseWheel);
 
     return () => {
-      // Remove event listener when component unmounts
+      // Remove event listener when the component unmounts
       sliderElement.removeEventListener("wheel", handleMouseWheel);
     };
   }, [activeIndex]);
 
   return (
+    <StagesWrapper id="scene-trigger" ref={scrollRef}
+    className="stage-slider-main"
+  >
     <Controller>
-     <Scene
-          triggerHook="onLeave"
-          duration={600}
-          pin
-        >
-    <StagesWrapper>
-      <Container className="stage-slider-main">
-        <div className="stages-header">
-          <h2>Stuck at any of these development stages?  We can help.</h2>
-          <p>
-            Optimize your idea at each step with our technological prowess and
-            business-critical insight.
-          </p>
-        </div>
-        <div className="slider-section">
-          <Row>
-            <Col lg={4} className="fixed-slider-column">
-              <div className="pagination fixed-slider-pagination">
-                <ul>
-                  {sliderItems.map((item, index) => (
-                    <li
-                      key={index}
-                      className={`pagination-item ${
-                        activeIndex === index ? "active" : ""
-                      }`}
-                      onClick={() => handlePaginationItemClick(index)}
-                    >
-                      {item.title}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </Col>
-            <Col lg={8} md={12} className="slider-column">
-              <Slider {...sliderSettings} ref={sliderRef}>
-                {sliderItems.map((item, index) => (
-                  <div key={index} className="slider-item">
-                    <Row>
-                      <Col lg={5} md={5}>
-                        <div className="title title-responsive">
-                          <h3>{item.title}</h3>
-                        </div>
-                        <img src={item.image} alt="project" />
-                      </Col>
-                      <Col lg={7} md={7} className="slider-column-text">
-                        <div className="title title-desktop">
-                          <h3>{item.title}</h3>
-                        </div>
-                        <div className="content">
-                          {item.content}
-                          <ul>
-                            {item.list.map((listItem, listIndex) => (
-                              <li key={listIndex}>{listItem}</li>
-                            ))}
-                          </ul>
-                        </div>
-                      </Col>
-                    </Row>
+      <Scene
+        triggerHook="onLeave"
+        duration={600}
+        offset={-50}
+        pin
+        classToggle="scroll-section" triggerElement="#scene-trigger"
+      >
+        <div>
+          <Container>
+            <div className="stages-header">
+              <h2>Stuck at any of these development stages? We can help.</h2>
+              <p>
+                Optimize your idea at each step with our technological prowess and
+                business-critical insight.
+              </p>
+            </div>
+            <div className="slider-section">
+              <Row>
+                <Col lg={4} className="fixed-slider-column">
+                  <div className="pagination fixed-slider-pagination">
+                    <ul>
+                      {sliderItems.map((item, index) => (
+                        <li
+                          key={index}
+                          className={`pagination-item ${
+                            activeIndex === index ? "active" : ""
+                          }`}
+                          onClick={() => handlePaginationItemClick(index)}
+                        >
+                          {item.title}
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                ))}
-              </Slider>
-            </Col>
-          </Row>
-        </div>
-      </Container>
-    </StagesWrapper>
-    </Scene>
-  </Controller>
+                </Col>
+                <Col lg={8} md={12} className="slider-column">
+                  <Slider {...sliderSettings} ref={sliderRef}>
+                    {sliderItems.map((item, index) => (
+                      <div key={index} className="slider-item">
+                        <Row>
+                          <Col lg={5} md={5}>
+                            <div className="title title-responsive">
+                              <h3>{item.title}</h3>
+                            </div>
+                            <img src={item.image} alt="project" />
+                          </Col>
+                          <Col lg={7} md={7} className="slider-column-text">
+                            <div className="title title-desktop">
+                              <h3>{item.title}</h3>
+                            </div>
+                            <div className="content">
+                              {item.content}
+                              <ul>
+                                {item.list.map((listItem, listIndex) => (
+                                  <li key={listIndex}>{listItem}</li>
+                                ))}
+                              </ul>
+                            </div>
+                          </Col>
+                        </Row>
+                      </div>
+                    ))}
+                  </Slider>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+          </div>
+          </Scene>
+    </Controller>
+        </StagesWrapper>
   );
 };
 
