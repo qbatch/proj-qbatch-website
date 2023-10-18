4a08c92cd50905d6462ac1faa51f1b27f0d5b69f
ali-haider-qbatch <ali@qbatch.com>
2023-10-18 17:16:24 +0500

    add-es-lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
+const path = require("path");
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  "node_modules",
+  "gatsby",
+  "dist",
+  "utils",
+  "eslint-rules"
+);
 module.exports = {
   siteMetadata: {
     robots: `index, follow`,
@@ -14,6 +23,19 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-plugin-styled-components',
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be omitted or customized
+        stages: ["develop"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        // Any additional eslint-webpack-plugin options below
+        // ...
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
