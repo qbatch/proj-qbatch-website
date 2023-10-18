5135bd6635976fa59e74318607f8fc5b12a7c62f
ali-haider-qbatch <ali@qbatch.com>
2023-10-18 15:34:40 +0500

    homepage/banner-added

--- src/components/PagesComponent/CompanyBanner/index.js
+++ src/components/PagesComponent/CompanyBanner/index.js
@@ -3,20 +3,18 @@ import React from "react";
 import Container from "../../UiComponent/Container";
 import Button from "../../UiComponent/Button";
 
-import BannerWrapper from "./style"
+import {BannerWrapper,H1} from "./style"
 
 const Index = () => (
     <BannerWrapper>
       <Container>
         <div className="banner-headings">
-          {["Team Spirit", "Great Work", "lasting Partnerships"].map((value, i) => <h2 key={i}>{value}</h2> )}
-          <h4>
-            As a close-knit team, we work hard to create products that
-             facilitate millions of people every day. 
-            Uncompromised quality is not our thing, and for this,
-             we heavily focus on building high-potential teams 
-            that believe in deriving real and lasting human experiences.
-            </h4>
+          {["Team Spirit", "Great Work", "lasting Partnerships"].map((value, i) => <H1 className={value.includes('lasting') ? "color-change" : ""} key={i}>{value}</H1> )}
+          <p>
+            As a close-knit team, we strive to create products that facilitate millions of people every
+            day. We heavily focus on building high-potential teams that believe in deriving real and
+            lasting human experiences.
+            </p>
         </div>
         <div className="banner-button">
           <Button text="Let’s Start Building" className="secondary-btn" />
--- src/components/PagesComponent/CompanyBanner/style.js
+++ src/components/PagesComponent/CompanyBanner/style.js
@@ -2,52 +2,39 @@ import styled from "styled-components";
 import { media } from "../../../theme/media-mixins";
 
 const BannerWrapper = styled.div`
-  background-image: url("/company-banner-img.png");
-  background-size: cover;
-  background-position: bottom;
-  padding: 97px 0;
+  background-image: url("/abouUs-banner.svg");
+  background-color: ${({ theme }) => theme.colors.secondaryColor};
+  background-position: 100% 0;
+  background-repeat: no-repeat;
+  padding: 132px 0 98px 0;
   ${media.sm`
     padding: 40px 0;
   `}
-  .banner-headings {
-    display: flex;
-    flex-wrap: wrap;
-    max-width: 900px;
-    h2 {
-      text-transform: uppercase;
-      display: flex;
-      align-items: center;
-      line-height: 52px;
-      margin: 0 16px 16px 0;
-      color: ${({ theme }) => theme.colors.whiteColor};
-      ${media.sm`
-        line-height: 32px;
-      `}
-      ${media.xs`
-        line-height: 24px;
-      `}
-      &::before {
-        content: "";
-        height: 2px;
-        width: 12px;
-        display: block;
-        background-color: ${({ theme }) => theme.colors.dangerColor};
-        margin: 0 16px 0 0;
-      }
-     
-    }
-    h4 {
-      line-height: 32px;
-      font-weight: 400;
-      letter-spacing: 0.48px;
-      color: ${({ theme }) => theme.colors.whiteColor};
-      margin-top: 16px;
-      max-width: 690px;
-    }
+  p{
+  color: ${({ theme }) => theme.colors.whiteColor};
+  font-size: ${({ theme }) => theme.fonts.baseFontSizeLg};
+  line-height:32px; 
+  max-width: 929px;
+  margin-top: 24px; 
   }
-  .banner-button {
-    padding-top: 42px;
+  .banner-button{
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    button{
+          margin-right: 116px;
+          margin-top: 37px;
+    }
   }
 `;
+const H1 = styled.h1`
+  font-size:96px;
+  color: ${({ theme }) => theme.colors.whiteColor};
+  line-height: 102px;
+  margin-bottom: 0px;
+  &.color-change{
+      color: ${({ theme }) => theme.colors.bodyLight};
+  }
+`
 
-export default BannerWrapper;
+export  {BannerWrapper,H1};
