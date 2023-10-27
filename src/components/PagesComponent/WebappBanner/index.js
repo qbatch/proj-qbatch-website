d027f42200a767ca3ba4943ef4aee0f1134dc962
Ahmad Siddiqui <ahmad.siddiqui@qbatch.com>
2023-10-27 17:25:28 +0500

    removed banner

--- src/components/PagesComponent/WebappBanner/index.js
+++ src/components/PagesComponent/WebappBanner/index.js
@@ -7,14 +7,7 @@ const index = () => {
     <>
       <WebappBanenrWrapper>
         <Container>
-          <div className='banner-headings'>
-            <h1 className='main-heading'>Custom Web Application Development Company </h1>
-            <h2>If You Think Web Apps Are Old-Fashioned. Think Again.</h2>
-            <p>88% of consumers use computers to research products and services before making a purchase. Don’t leave your potential customers confused, instead amuse them with a stunning web appearance!</p>
-          </div>
-          <div className='banner-button'>
-            <Button text="Let’s Start Building" className="primary-btn" />
-          </div>
+         
         </Container>
       </WebappBanenrWrapper>
     </>
