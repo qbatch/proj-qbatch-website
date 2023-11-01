c2573594c02bbfbc74a353744ae18fb6ec27a5b5
ateeq-rehman-qbatch <ateeq.rehman@qbatch.com>
2023-11-01 11:39:00 +0500

    remove scroll effect in marketplace section

--- src/components/PagesComponent/MarketplaceIntegrations/index.js
+++ src/components/PagesComponent/MarketplaceIntegrations/index.js
@@ -1,9 +1,8 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { Col, Container, Row } from 'react-bootstrap'
-import VisibilitySensor from "react-visibility-sensor";
 
 import { marketPlaceData } from '../../../constants'
 
@@ -14,33 +13,7 @@ const Index = () => {
   const sliderRef2 = useRef(null)
   const scrollRef2 = useRef(null)
   const [activeIndex, setActiveIndex] = useState(0);
-  const [prevVisibility, setPrevVisibility] = useState(false);
 
-  const handleVisibilityChange = (isVisible) => {
-    // If the section becomes visible or when scrolling from top or bottom
-    if (isVisible || (isVisible && !prevVisibility)) {
-      scrollToSectionStart(isVisible);
-    }
-    setPrevVisibility(isVisible); // Store the previous visibility state
-  };
-  const handleMouseEnter = () => {
-    document.body.style.overflow = "hidden";
-    scrollToSectionStart();
-  };
-  const handleMouseLeave = () => {
-    document.body.style.overflow = "auto";
-  };
-
-  const scrollToSectionStart = () => {
-    if (scrollRef2.current) {
-      const sectionTop = scrollRef2.current.offsetTop
-      const sectionTopCalc = sectionTop - 80
-      window.scrollTo({
-        top: sectionTopCalc,
-        behavior: 'smooth',
-      })
-    }
-  }
   const sliderSettings = {
     dots: false,
     infinite: false,
@@ -66,57 +39,11 @@ const Index = () => {
   const handlePaginationItemClick = (index) => {
     sliderRef2.current.slickGoTo(index)
   }
-
-  useEffect(() => {
-    const handleMouseWheel = (e) => {
-      const isAtFirstSlide = activeIndex === 0
-      const isAtLastSlide = activeIndex === marketPlaceData.length - 1
-
-      if (e.deltaY > 0) {
-        if (!isAtLastSlide) {
-          e.preventDefault()
-          sliderRef2.current.slickNext()
-          scrollToSectionStart()
-          document.body.style.overflow = "hidden";
-        } else {
-          setTimeout(() => {
-            document.body.style.overflow = "auto";
-          }, 1000);
-        }
-      } else if (e.deltaY < 0) {
-        if (isAtFirstSlide) {
-          setTimeout(() => {
-            document.body.style.overflow = "auto";
-          }, 1000);
-        } else {
-          e.preventDefault()
-          sliderRef2.current.slickPrev()
-          scrollToSectionStart()
-          document.body.style.overflow = "hidden";
-        }
-      }
-    }
-
-    // Add event listener for mousewheel
-    const sliderElement = document.querySelector('.stage-slider-main2')
-    sliderElement.addEventListener('wheel', handleMouseWheel)
-
-    return () => {
-      // Remove event listener when the component unmounts
-      sliderElement.removeEventListener('wheel', handleMouseWheel)
-    }
-  }, [activeIndex])
   const marketplace = marketPlaceData.find((x) => x.content);
   return (
-    <VisibilitySensor onChange={handleVisibilityChange}
-    partialVisibility={true}
-    offset={{bottom:300, top:300}}
-    >
     <StagesWrapper
       id="scene-trigger2"
       ref={scrollRef2}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
       className="stage-slider-main2"
       marketplace={marketplace ? '' : '49px'}
     >
@@ -166,7 +93,6 @@ const Index = () => {
         </Container>
       </div>
     </StagesWrapper>
-    </VisibilitySensor>
   )
 }
 
