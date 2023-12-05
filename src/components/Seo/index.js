3d1dff080f2b9fe745ccafe4c65c5ec40205d23f
ali-haider-qbatch <ali@qbatch.com>
2023-12-05 13:19:54 +0500

    og-twitter-tags-added

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSiteMetadata } from '../../hooks/use-site-metadata.jsx'
 
-const SEO = ({ title, description, language, robots, pathname, children }) => {
+const SEO = ({ title, description, language, robots, pathname, children, type, data }) => {
   const {
     title: defaultTitle,
     description: defaultDescription,
@@ -19,11 +19,22 @@ const SEO = ({ title, description, language, robots, pathname, children }) => {
     language: language || defaultLanguage,
     keywords,
   }
+
   return (
     <>
       <title>{seo.title}</title>
       <meta name="robots" content={seo.robots} />
       <meta name="description" content={seo.description} />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta property="og:url" content={seo.url} key="og:url" />
+      <meta property="og:title" content={seo.title} key="og:title" />
+      <meta property="og:description" content={seo.description} key="og:description" />
+      <meta property="og:image" content={seo.image} key="og:image" />
+      <meta name="twitter:card" content="summary_large_image" key="twitter:card" />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <meta name="twitter:creator" content={seo.twitterUsername} />
       <link rel="canonical" href={seo.url} />
       <meta name="keywords" content={seo.keywords} />
       <meta http-equiv="content-language" content={seo.language} />
