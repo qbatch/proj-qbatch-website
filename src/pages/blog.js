9ad616f07e0a28be3d743c3022914ab9063bf22e
ateeq-rehman-qbatch <ateeq.rehman@qbatch.com>
2024-04-08 14:15:56 +0500

    remove unused draft package

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -35,7 +35,7 @@ const BlogPage = ({ pageContext }) => {
   function capitalizeFirstLetter(str) {
     return str?.charAt(0).toUpperCase() + str?.slice(1)
   }
-  
+
   return (
     <Layout>
       <BlogBanner />
@@ -95,7 +95,7 @@ export const Head = () => {
   const seoData = categoryPath ? categoryFilter[0]?.seo : blogSeo.allStrapiBlog.nodes[0]?.seo
   return (
     <SEO
-      // title={seoData.metaTitle}
+      title={seoData.metaTitle}
       description={seoData?.metaDescription}
       keywords={seoData?.keywords}
       language={seoData?.language}
