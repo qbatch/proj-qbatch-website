e2b871a2dc81268ea7a494e83df384a417cd4297
ali-haider-qbatch <ali@qbatch.com>
2024-04-16 17:25:50 +0500

    build-issue-solved

--- src/pages/blogDetails.js
+++ src/pages/blogDetails.js
@@ -57,7 +57,7 @@ const BlogDetails = ({ pageContext }) => {
       </ContentWrapper>
       <BlogDetailBanner data={blogData} />
       <BlogDetailsContent data={blogData} path={path} />
-       {blogData.seo.structuredData ? <Script type="application/ld+json">{schemaAsString}</Script>: null  } 
+       {blogData.seo?.structuredData ? <Script type="application/ld+json">{schemaAsString}</Script>: null  } 
     </Layout>
   )
 }
