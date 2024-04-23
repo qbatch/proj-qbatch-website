cd2e310779a5263db01f1a8be67ce146edaf4143
kalash-qbatch <105213530+kalash-qbatch@users.noreply.github.com>
2024-04-23 18:16:15 +0500

    About us page UI issues fixed

--- src/components/PagesComponent/ValueSystem/style.js
+++ src/components/PagesComponent/ValueSystem/style.js
@@ -12,7 +12,16 @@ const ValueSystemWrapper = styled.div`
       &:first-child {
         display: block;
         margin-bottom: 24px;
+        font-weight:500;
+        text-transform:none;
+        ${media.sm`
+        margin-bottom:12px;
+        
+        `}
       }
+    }
+    .text-lowercase{
+      font-weight:300;
     }
     .value-system {
       font-weight: ${({ theme }) => theme.fontWeight.bold};
@@ -24,57 +33,63 @@ const ValueSystemWrapper = styled.div`
     ${media.sm`
      padding-top: 26px;
     `}
-  }
-  .value-col {
-    padding: 36px 4.2% 20px;
-    min-height: 238px;
-    display: flex;
-    align-items: flex-start;
-    border-right: 1px solid ${({ theme }) => theme.colors.dividerColor};
-    border-bottom: 1px solid ${({ theme }) => theme.colors.dividerColor};
-    .value-icon {
-      height: 60px;
-      width: 60px;
-    }
-    .col-inner {
-      padding-left: 16px;
-      padding-top: 12px;
-      h3 {
-        margin-bottom: 16px;
-      }
-      p {
-        line-height: 24px;
-      }
-    }
-    &:nth-child(3n) {
-      border-right: 0;
-    }
-    &:nth-child(7),
-    &:nth-child(8),
-    &:nth-child(9) {
-      border-bottom: 0;
-    }
-    ${media.lg`
-    &:nth-child(even){
-      border-right: 0;
-     }
-     &:nth-child(odd){
+    .value-col {
+      padding: 36px 4.2% 20px;
+      min-height: 238px;
+      display: flex;
+      align-items: flex-start;
       border-right: 1px solid ${({ theme }) => theme.colors.dividerColor};
-     }
-     &:nth-child(7),
-     &:nth-child(8) {
-      border-bottom:  1px solid ${({ theme }) => theme.colors.dividerColor};
-     }
-    `}
-    ${media.md`
-    min-height: 170px;
-    
-    `}
-    ${media.sm`
-    min-height: auto;
-    padding: 26px;
-      border-right: 0 !important;
-    `}
+      border-bottom: 1px solid ${({ theme }) => theme.colors.dividerColor};
+      ${media.sm`
+      padding: 16px !important;
+      `}
+      .value-icon {
+        height: 60px;
+        width: 60px;
+      }
+      .col-inner {
+        padding-left: 16px;
+        padding-top: 12px;
+        h3 {
+          margin-bottom: 16px;
+          ${media.sm`
+      margin-bottom: 8px !important;
+      `}
+        }
+        p {
+          line-height: 24px;
+        }
+      }
+      &:nth-child(3n) {
+        border-right: 0;
+      }
+      &:nth-child(7),
+      &:nth-child(8),
+      &:nth-child(9) {
+        border-bottom: 0;
+      }
+      ${media.lg`
+      &:nth-child(even){
+        border-right: 0;
+       }
+       &:nth-child(odd){
+        border-right: 1px solid ${({ theme }) => theme.colors.dividerColor};
+       }
+       &:nth-child(7),
+       &:nth-child(8) {
+        border-bottom:  1px solid ${({ theme }) => theme.colors.dividerColor};
+       }
+      `}
+      ${media.md`
+      min-height: 170px;
+      
+      `}
+      ${media.sm`
+      min-height: auto;
+      padding: 26px;
+        border-right: 0 !important;
+      `}
+    }
   }
 `
 
