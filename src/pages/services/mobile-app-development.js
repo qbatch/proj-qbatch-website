a1c6bde27ed868ed989173abef479946929e3c6b
ali-haider-qbatch <ali@qbatch.com>
2024-04-26 14:04:56 +0500

    banner-update

--- src/pages/services/mobile-app-development.js
+++ src/pages/services/mobile-app-development.js
@@ -22,7 +22,7 @@ import Banner from "../../components/PagesComponent/PageBanner";
 import CrewNotVendors from "../../components/PagesComponent/CrewNotVendors";
 import DedicatedMobileDevelopers from "../../components/PagesComponent/DedicatedMobileDevelopers";
 import MultiplePlatformSolutions from "../../components/PagesComponent/MultiplePlatformSolutions";
-import BannerImage from "../../../static/mobile-app-banner.webp";
+import BannerImage from "../../../static/mobile-app-banner.svg";
 
 import {
   lostToLaunchedItems,
@@ -50,7 +50,7 @@ const MobileApp = () => {
           heading="Deliver Powerful Mobile App Experiences"
           paragraph="We build dynamic and future-ready mobile apps for startups and large-scale enterprises across devices and operating systems."
           buttonText="Kickstart Your Transformation Journey"
-          wrapperClass="cto-banner change-background-size"
+          wrapperClass="cto-banner"
           blogInner={BannerImage}
           mobileViewBanner={BannerImage}
           customCrumbs={[
