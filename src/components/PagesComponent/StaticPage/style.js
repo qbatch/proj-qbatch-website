fa2609400f04b9a73b54a730f47602e762f5a5f4
AhmadSiddiquii <89375837+AhmadSiddiquii@users.noreply.github.com>
2024-05-29 16:04:50 +0500

    font consistency fix

--- src/components/PagesComponent/StaticPage/style.js
+++ src/components/PagesComponent/StaticPage/style.js
@@ -2,22 +2,44 @@ import styled from "styled-components";
 import { media } from "../../../theme/media-mixins";
 
 const StaticPageWrapper = styled.div`
-  padding: 120px 0;
+  padding: 24px 0 120px;
   ${media.sm`
-    padding: 60px 0;
+    padding: 24px 0 60px;
   `}
 
-  h3 {
+  h2, .text-h2 {
     margin-bottom: 24px;
+    margin-top: 48px;
+    ${media.sm`
+    margin-top: 24px;
+    `}
   }
   h4 {
-    font-size: ${({ theme }) => theme.fonts.baseFontSizeSm};
+    font-size: ${({ theme }) => theme.fonts.baseFontSizeLg};
+    margin-top: 24px;
+    margin-bottom: 16px;
   }
-  p {
-    font-size: ${({ theme }) => theme.fonts.baseFontSize};
-    line-height: 24px;
+  p{
+    margin-bottom: 24px;
+    strong{
+      font-weight:600;
+    }
   }
-  
+  p, li{
+    font-size: ${({ theme }) => theme.fonts.baseFontSizeLg};
+    line-height: 32px;
+    color: #434343;
+    ${media.sm`
+    font-size: ${({ theme }) => theme.fonts.baseFontSizeMediumSmal};
+    line-height: 20px;
+    `}
+  }
+  ul{
+    margin-top: 24px;
+  }
+  hr{
+    opacity: 1;
+  }
   ul {
     list-style: disc;
     padding-left: 32px;
