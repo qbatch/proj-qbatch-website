a3b6d84e0cfda592b89b03a996bdb4ee811782a2
AhmadSiddiquii <89375837+AhmadSiddiquii@users.noreply.github.com>
2024-05-31 16:23:40 +0500

    slider spacing add

--- src/components/UiComponent/Slider/style.js
+++ src/components/UiComponent/Slider/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { media } from '../../../theme/media-mixins'
 const SliderWrapper = styled.div`
-  padding: 96px 0;
+  padding: 96px 0 110px;
   ${media.sm`
     padding: 56px 0;
   `}
