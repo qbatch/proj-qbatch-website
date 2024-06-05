9bf42144efba3286424094d518323f7d603bf700
ali-haider-qbatch <ali@qbatch.com>
2024-06-05 15:48:46 +0500

    schema-index

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -22,6 +22,17 @@ const SeoComponent = ({ seoData, schemaData }) => {
             {JSON.stringify(data.childStrapiComponentSchemaSchemaStructureddataJsonnode)}
           </script>
         ))}
+                <script type="application/ld+json">
+          {JSON.stringify(
+            {
+              "potentialAction": {
+                "name": "Book a Demo",
+                "@type": "Action",
+                "target": "https://qbatch.com/contact/"
+              }
+              }
+            )}
+          </script>
     </SEO>
   );
 };
