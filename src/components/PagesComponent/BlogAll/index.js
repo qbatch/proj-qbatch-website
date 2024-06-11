11ddb0a4763cb824110ad2aba478dff3b40cbee5
AhmadSiddiquii <89375837+AhmadSiddiquii@users.noreply.github.com>
2024-06-11 15:32:33 +0500

    blog article seo

--- src/components/PagesComponent/BlogAll/index.js
+++ src/components/PagesComponent/BlogAll/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { navigate } from "gatsby";
+import { navigate, Link } from "gatsby";
 import { Col, Row } from "react-bootstrap";
 
 import WeCanHelp from "../../PagesComponent/WeCanHelp";
@@ -23,64 +23,62 @@ const index = ({ data }) => {
         {data
           .slice(-1)
           .map((item, ind) => (
-          <InnerBannerWrapper key={ind}>
-            <Row>
-              <Col lg={7}>
-                <img className="blog-image" src={item.blogImg?.localFile.url} />
-              </Col>
-              <Col lg={5}>
-                <div className="blog-wrapper">
-                  {item.blogTags && (
-                    <div className="blog-badge">
-                      {item.blogTags?.strapi_json_value[0]}
+            <InnerBannerWrapper key={ind}>
+              <Row>
+                <Col lg={7}>
+                  <img className="blog-image" src={item.blogImg?.localFile.url} />
+                </Col>
+                <Col lg={5}>
+                  <div className="blog-wrapper">
+                    {item.blogTags && (
+                      <div className="blog-badge">
+                        {item.blogTags?.strapi_json_value[0]}
+                      </div>
+                    )}
+                    <div className="blog-title">
+                      <Link to={`/blog/${item.seo.slug}/`} state={{ blogId: item.id }}>
+                        <h2>{item.blogTitle}</h2>
+                      </Link>
+                      <div className="banner-desc">
+                        <p>{stripHtmlTags(item.blogDescription?.data.blogDescription)}</p>
+                      </div>
                     </div>
-                  )}
-                  <div className="blog-title">
-                    <h2 onClick={() => {
-                        navigate(`/blog/${item.seo.slug}/`, {
-                          state: { blogId: item.id },
-                        });
-                      }}>{item.blogTitle}</h2>
-                    <div className="banner-desc">
-                    <p>{stripHtmlTags(item.blogDescription?.data.blogDescription)}</p>
-                    </div>
-                  </div>
-                  <div className="d-flex justify-content-between flex-wrap gap-3">
-                    <div
-                      className="author-name pointer"
-                      onClick={() => {
-                        navigate(`/authors/${item?.user?.username}/`, {
-                          state: { slug: item.seo.slug },
-                        });
-                      }}
-                    >
-                      <span>
-                        {" "}
-                        <img
-                          src={item?.user?.image?.localFile?.url || "/avatar.svg"}
-                          width="24px"
-                          height="24px"
-                          alt="no-user"
-                          className="avatar-sm-img"
-                        />{" "}
-                        {item.user?.name || "No User"}
-                      </span>
-                    </div>
-                    <div className="timer">
-                      <img src="/timer.svg" alt="timer" width={16} height={16} />
-                      <div>
-                        <ReadingTime
-                          description={item.blogDescription?.data.blogDescription}
-                        />{" "}
-                        Minutes Read
+                    <div className="d-flex justify-content-between flex-wrap gap-3">
+                      <div
+                        className="author-name pointer"
+                        onClick={() => {
+                          navigate(`/authors/${item?.user?.username}/`, {
+                            state: { slug: item.seo.slug },
+                          });
+                        }}
+                      >
+                        <span>
+                          {" "}
+                          <img
+                            src={item?.user?.image?.localFile?.url || "/avatar.svg"}
+                            width="24px"
+                            height="24px"
+                            alt="no-user"
+                            className="avatar-sm-img"
+                          />{" "}
+                          {item.user?.name || "No User"}
+                        </span>
+                      </div>
+                      <div className="timer">
+                        <img src="/timer.svg" alt="timer" width={16} height={16} />
+                        <div>
+                          <ReadingTime
+                            description={item.blogDescription?.data.blogDescription}
+                          />{" "}
+                          Minutes Read
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </Col>
-            </Row>
-          </InnerBannerWrapper>
-        ))}
+                </Col>
+              </Row>
+            </InnerBannerWrapper>
+          ))}
       </Container>
       <Divider margin="64px 0" />
       <Container>
