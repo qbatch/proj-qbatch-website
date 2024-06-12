5758040ed5a2f70dfbbb353c83af0d9d53114098
AhmadSiddiquii <89375837+AhmadSiddiquii@users.noreply.github.com>
2024-06-12 13:19:26 +0500

    schema null value remove

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import SEO from '../components/Seo'
+import SEO from '../components/Seo';
 import { replaceUnderscoreWithAt } from '../constants/Utils';
 
+const removeNullValues = (obj) => {
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != null));
+};
+
 const SeoComponent = ({ seoData, schemaData }) => {
+  
   const transformedObject = replaceUnderscoreWithAt(schemaData);
+ 
+
+  const filteredObject = transformedObject
+    .filter(x => x && x.visibilityIn && x.childStrapiComponentSchemaSchemaStructureddataJsonnode)
+    .map(x => ({
+      ...x,
+      childStrapiComponentSchemaSchemaStructureddataJsonnode: removeNullValues(x.childStrapiComponentSchemaSchemaStructureddataJsonnode)
+    }));
+    console.log(filteredObject);
 
   return (
     <SEO
@@ -15,13 +29,11 @@ const SeoComponent = ({ seoData, schemaData }) => {
       image={seoData.metaimage[0].localFile.url}
       pathname={`/services${seoData.slug}`}
     >
-      {transformedObject
-        .filter((x) => x.visibilityIn)
-        .map((data, i) => (
-          <script key={i} type="application/ld+json">
-            {JSON.stringify(data.childStrapiComponentSchemaSchemaStructureddataJsonnode)}
-          </script>
-        ))}
+      {filteredObject.map((data, i) => (
+        <script key={i} type="application/ld+json">
+          {JSON.stringify(data.childStrapiComponentSchemaSchemaStructureddataJsonnode)}
+        </script>
+      ))}
     </SEO>
   );
 };
