c2e1fa2e97b664d2006a80b0b85c29648b2a9f93
ateeq-rehman-qbatch <ateeq.rehman@qbatch.com>
2024-07-09 12:48:20 +0500

    add loading screen and fix fav icon issues

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -17,11 +17,10 @@ const Layout = ({ children }) => {
         <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@300;400;600;700&display=swap" rel="stylesheet" />
         <link rel="icon" href="/fav-icon.png" type="image/png" />
         <link rel="shortcut icon" href="/fav-icon.png" type="image/png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/fav-icon.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/fav-icon16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/fav-icon.png" />
         <link rel="icon" href="/fav-icon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/fav-icon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/fav-icon.png" />
         <html lang='en-US' />
       </Helmet>
       <GlobalStyle />
