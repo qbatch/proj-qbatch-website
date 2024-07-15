b3e431c82a027fba76c204cba4e556601ed1a8a1
AhmadSiddiquii <89375837+AhmadSiddiquii@users.noreply.github.com>
2024-07-15 18:06:52 +0500

    redirect fix

--- src/components/PagesComponent/BlogDetailsContent/index.js
+++ src/components/PagesComponent/BlogDetailsContent/index.js
@@ -237,7 +237,7 @@ function App({ data, path }) {
                               alt="no-avatar"
                             />
                             <span className="avatar-name">
-                              <a href={`/authors/${data?.user?.username}`}>
+                              <a href={`/authors/${data?.user?.username}/`}>
                                 {data?.user?.name}
                               </a>
                             </span>
@@ -275,7 +275,7 @@ function App({ data, path }) {
                               ))}
                             </ul>
                             <div>
-                              <a href={`/authors/${data?.user?.username}`}>
+                              <a href={`/authors/${data?.user?.username}/`}>
                                 <img
                                   src="/author-name-arrow.svg"
                                   className="pointer"
@@ -291,7 +291,7 @@ function App({ data, path }) {
                                 <span className="inner-heading">Contributors:</span>
                                 <div className="contributors">
                                   {data?.contributor?.map((contributor, index) => (
-                                    <a href={`/authors/${contributor.username}`} key={index} title={contributor.username}>
+                                    <a href={`/authors/${contributor.username}/`} key={index} title={contributor.username}>
                                       <img src={contributor.image?.localFile.url} />
                                     </a>
                                   ))}
@@ -362,7 +362,7 @@ function App({ data, path }) {
                                   <div
                                     className="d-flex align-items-center gap-2 pointer"
                                     onClick={() => {
-                                      navigate(`/authors/${card?.user?.username}`, {
+                                      navigate(`/authors/${card?.user?.username}/`, {
                                         state: { slug: card.seo.slug },
                                       });
                                     }}
