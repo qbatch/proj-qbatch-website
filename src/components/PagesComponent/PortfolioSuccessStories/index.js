9e04defc699b68f4619c3a49ca2049f969b1e631
ateeq-rehman-qbatch <ateeq.rehman@qbatch.com>
2024-10-07 12:58:51 +0500

    remove useMemo

--- src/components/PagesComponent/PortfolioSuccessStories/index.js
+++ src/components/PagesComponent/PortfolioSuccessStories/index.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import { graphql, Link, useStaticQuery } from 'gatsby';
