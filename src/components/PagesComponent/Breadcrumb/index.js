eb1295206e32907f8956d20a9be7b9106983c055
kalash-qbatch <105213530+kalash-qbatch@users.noreply.github.com>
2024-11-25 18:26:53 +0500

    breadcrumbs updated

--- src/components/PagesComponent/Breadcrumb/index.js
+++ src/components/PagesComponent/Breadcrumb/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { useLocation } from '@reach/router'; 
+import { useLocation } from '@reach/router';
 import BreadcrumbWrapper from './style';
 
 const Breadcrumb = ({ crumbs, wrapperClass }) => {
 
-  const location =  useLocation();
+  const location = useLocation();
 
   return (
     <BreadcrumbWrapper className={wrapperClass}>
       <div className="breadcrumb">
         <ul className="d-flex gap-2 ps-0">
-          {crumbs.map((crumb, index) => (
-            <li key={index} className={location.pathname === crumb.pathname ? 'current-breadcrumb' : ''}>
+          {crumbs.map((crumb, index) => {
+            const isCurrentPath = location.pathname.replace(/\/$/, '') === crumb.pathname.replace(/\/$/, '');
+            return (<li key={index} className={isCurrentPath ? 'current-breadcrumb' : ''}>
               <Link className="pointer" to={crumb.pathname}>
                 <span>{crumb.crumbLabel}</span>
                 {index < crumbs.length - 1 && <span className="ps-1">{crumb.crumbSeparator}</span>}
               </Link>
-            </li>
-          ))}
+            </li>)
+          })}
         </ul>
       </div>
     </BreadcrumbWrapper>
